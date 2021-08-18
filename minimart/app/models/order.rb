97783b3d8a5833648deff16eb4dade80de702df2
MinamiInoue2323 <inoue@ailab.ics.keio.ac.jp>
2021-08-18 17:27:28 +0900

    17:30

--- minimart/app/models/order.rb
+++ minimart/app/models/order.rb
@@ -2,5 +2,6 @@ class OrderItem < ApplicationRecord
     #dependent-pickuplocationをデストロイする時に依存関係のある外部キーなどが合った場合の挙動
     has_many :order_item, dependent: :restrict_with_exception
     belongs_to :product, optional: true
+    belongs_to :user, optional: true
   end
   
--- minimart/app/models/order_item.rb
+++ minimart/app/models/order_item.rb
@@ -2,4 +2,5 @@ class OrderItem < ApplicationRecord
   #dependent-pickuplocationをデストロイする時に依存関係のある外部キーなどが合った場合の挙動
   #has_many :orders, dependent: :restrict_with_exception
   belongs_to :product, optional: true
+  belongs_to :order, optional: true
 end
