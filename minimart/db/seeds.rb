792998db2080617097db3aee785181b142a159cc
Tomo Watari <tomowatari@TomonoMacBook-Pro.local>
2021-08-18 16:43:47 +0900

    add seed

--- minimart/db/seeds.rb
+++ minimart/db/seeds.rb
@@ -8,11 +8,16 @@
 
 pickup_location = PickupLocation.create!(name: 'WeWork みなとみらい')
 
-9.times do |i|
-  PickupLocation.create!(name: "受け取り場所 #{i}")
-end
-
 user = User.create!(
   name: 'tomart',
   pickup_location: pickup_location,
 )
+
+
+9.times do |i|
+  PickupLocation.create!(name: "受け取り場所 #{i}")
+
+  Product.create!(name: "Good Product #{i}", description: "Nice Description #{i}", price: i * 100, image_url: "goodimage/#{i}")
+  Order.create!(user_id: 1, pickup_location_id: 1, total_amount: i * 5, ordered_at: "2021-08-18", delivery_date: "2021-08-19")
+  OrderItem.create(product_id: i, quantity: i * 3, order_id: i)
+end
