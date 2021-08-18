f71ff82a94284e39e025ccec42ff868277c6d7d8
chamegashi <msymsy0693wrb@gmail.com>
2021-08-18 13:53:00 +0900

    query pickupLocation

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -12,6 +12,12 @@ module Types
       context[:current_user]
     end
 
+    field :pickupLocations, [Types::PickupLocationType], null:true
+
+    def pickupLocations
+      PickupLocation.all
+    end
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
