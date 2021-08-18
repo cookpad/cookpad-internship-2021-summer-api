f2bf76e37ac5f173d03ec8153142e83c3756a07b
rakutek <rakutek12@gmail.com>
2021-08-18 13:41:03 +0900

    fix user_type

--- minimart/app/graphql/types/user_type.rb
+++ minimart/app/graphql/types/user_type.rb
@@ -3,7 +3,5 @@ module Types
     field :id, ID, null: false
     field :pickup_location_id, Integer, null: true
     field :name, String, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
