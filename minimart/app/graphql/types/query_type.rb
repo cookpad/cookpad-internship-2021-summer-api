58229d7db4646d7537f88946d4664e3a3ca57b37
Naoki08 <wnp1998@gmail.com>
2021-08-18 13:59:21 +0900

    ハンズオンとplaygroundの導入

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -7,6 +7,11 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :viewer, Types::UserType, null: true
+
+    def viewer
+      context[:current_user]
+    end
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
