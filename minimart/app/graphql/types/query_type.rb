367f157c631dcd855ca5617491c0c41d976748c5
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-08-18 13:58:26 +0900

    query(:ID){id}} で引数の商品IDの商品を返す

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -22,10 +22,24 @@ module Types
 
     def products
       products = Product.all.map do |product|
-        product.image_url = image_url(product.image_url)
+        product.image_url = change_image_url(product.image_url)
         product
       end
       products
+    end
+
+    field :product, Types::ProductType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def product(id:)
+      if Product.find_by(id: id)
+        product = Product.find_by(id: id)
+        product.image_url = change_image_url(product.image_url)
+        product
+      else
+        raise GraphQL::ExecutionError, "Product is not found!"
+      end
     end
 
     # TODO: remove me
@@ -37,7 +51,7 @@ module Types
 
     private
 
-    def image_url(image_url)
+    def change_image_url(image_url)
       image_path = "/images/products/#{image_url}"
       File.join(context[:image_base_url], image_path)
     end
