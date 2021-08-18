d49ff313d95cc0c5df167d0b57ba8877a9dcd8a4
MinamiInoue2323 <inoue@ailab.ics.keio.ac.jp>
2021-08-18 15:05:22 +0900

    finish handson

---
+++ minimart/app/graphql/mutations/update_pickup_location.rb
@@ -0,0 +1,16 @@
+module Mutations
+  class UpdatePickupLocation < BaseMutation
+    # TODO: define return fields
+    field :pickup_location, Types::PickupLocationType, null: true
+
+    # TODO: define arguments(引数を定義)
+    argument :pickup_location_id, ID, required: true
+
+    # TODO: define resolve method
+    def resolve(pickup_location_id:)
+      pickup_location = PickupLocation.find_by(id: pickup_location_id)
+      context[:current_user].update(pickup_location: pickup_location)
+      {pickup_location: pickup_location}
+    end
+  end
+end
