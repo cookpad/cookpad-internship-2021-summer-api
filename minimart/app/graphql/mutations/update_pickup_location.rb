d075843e0a736e2441a1e78c2997c617baa376f9
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-08-18 13:48:28 +0900

    不要なコードの削除

--- minimart/app/graphql/mutations/update_pickup_location.rb
+++ minimart/app/graphql/mutations/update_pickup_location.rb
@@ -2,13 +2,13 @@ module Mutations
   class UpdatePickupLocation < BaseMutation
     field :pickup_location, Types::PickupLocationType, null: true
 
-    argument :pickup_location_id, ID, required: true
+    argument :pickup_location_id, ID, required: true # 引数
 
     def resolve(pickup_location_id:)
       pickup_location = PickupLocation.find_by(id: pickup_location_id)
       context[:current_user].update(pickup_location: pickup_location)
       { pickup_location: pickup_location }
     end
-    
+
   end
 end
--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -6,17 +6,11 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-    field :viewer, Types::UserType, null: true
+    field :viewer, Types::UserType, null: true, description: "リクエストしたユーザー自身を返す"
 
     def viewer
       context[:current_user]
     end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
   end
 end
