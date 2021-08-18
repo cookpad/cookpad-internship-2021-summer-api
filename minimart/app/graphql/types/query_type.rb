17e932a789baa2b52fc23bf9131cfb0954933831
rakutek <rakutek12@gmail.com>
2021-08-18 14:42:03 +0900

    query pickupLocationsの実装

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -7,9 +7,15 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :viewer, Types::UserType, null: true
+    
+    field :pickupLocations, [Types::PickupLocationType], null: false
 
     def viewer
       context[:current_user]
+    end
+    
+    def pickupLocations
+      PickupLocation.all
     end
 
   end
