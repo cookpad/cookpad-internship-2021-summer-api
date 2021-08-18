ecbd6a36bff8e700e86e0ffc29a753134d2f5919
rakutek <rakutek12@gmail.com>
2021-08-18 13:30:57 +0900

    updatePickupLocation の実装

--- minimart/app/graphql/types/user_type.rb
+++ minimart/app/graphql/types/user_type.rb
@@ -1,6 +1,9 @@
 module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
+    field :pickup_location_id, Integer, null: true
     field :name, String, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
