f700adaadad993eadc318821d5bbba4db6cfd294
Tomo Watari <tomowatari@TomonoMacBook-Pro.local>
2021-08-18 10:46:29 +0900

    hands-on accomplished

---
+++ minimart/app/graphql/mutations/update_pickup_location.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class UpdatePickupLocation < BaseMutation
+    field :pickup_location, Types::PickupLocationType, null: true
+
+    argument :pickup_location_id, ID, required: true
+
+    def recolve(pickup_location_id:)
+      pickup_location = PickupLocation.find_by(id: pickup_location_id)
+      if pickup_location.nil?
+        raise GraphQL::ExecutionError, pickup_location.errors.full_messages.join(", ")
+      end
+
+      context[:current_user].update(pickup_location: pickup_location)
+      { pickup_location: pickup_location }
+    end
+  end
+end
