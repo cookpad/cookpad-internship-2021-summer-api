1538400e09389a11a2eb7b63bd50c57d69a97a1f
s-usukura <81461525+s-usukura@users.noreply.github.com>
2021-08-18 17:07:47 +0900

    基本課題productsクエリ（商品URLの部分は除く）まで実装

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -12,6 +12,17 @@ module Types
       context[:current_user]
     end
 
+    field :pickup_locations, [Types::PickupLocationType], null: false
+    def pickup_locations
+      PickupLocation.all
+    end
+
+
+    field :products, [Types::ProductType], null: false
+    def products
+      Product.all
+    end
+
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
