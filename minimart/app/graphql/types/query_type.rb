80a225339599db8106405fbdd383c53c55da3c87
rakutek <rakutek12@gmail.com>
2021-08-18 13:29:57 +0900

    viewer の実装

--- minimart/app/graphql/types/query_type.rb
+++ minimart/app/graphql/types/query_type.rb
@@ -6,12 +6,11 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :viewer, Types::UserType, null: true
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def viewer
+      context[:current_user]
     end
+
   end
 end
