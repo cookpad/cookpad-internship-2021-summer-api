727a6fc4cc01766c9acd2eae8fdc9fec3af67723
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-08-20 18:18:20 +0900

    Add field `categories` to Product

--- minimart/app/graphql/types/product_type.rb
+++ minimart/app/graphql/types/product_type.rb
@@ -9,9 +9,14 @@ module Types
     field :description, String, '商品の詳細', null: false
     field :price, Integer, '商品の価格', null: false
     field :image_url, String, '商品画像の URL', null: false
+    field :categories, [Types::CategoryType], '商品の属するカテゴリ', null: false
 
     def image_url
       File.join(context[:image_base_url].to_s, object.image_path)
+    end
+
+    def categories
+      Loaders::AssociationLoader.for(Product, :categories).load(object)
     end
   end
 end
--- minimart/spec/graphql/queries/product_spec.rb
+++ minimart/spec/graphql/queries/product_spec.rb
@@ -10,12 +10,27 @@ RSpec.describe 'GraphQL query: product(id: ID!)' do
           description
           price
           imageUrl
+          categories {
+            name
+          }
         }
       }
     GRAPHQL
   end
   let(:variables) { { id: MinimartSchema.id_from_object(product, Types::ProductType) } }
-  let(:product) { create(:product, name: 'トマト', description: '新鮮なトマト', price: 150, image_path: '/tomato.jpg') }
+  let(:product) do
+    create(
+      :product,
+      name: 'トマト',
+      description: '新鮮なトマト',
+      price: 150,
+      image_path: '/tomato.jpg',
+      categories: [
+        create(:category, name: '野菜'),
+        create(:category, name: '実もの'),
+      ],
+    )
+  end
 
   it 'returns a product' do
     result = MinimartSchema.execute(query, variables: variables)
@@ -29,6 +44,10 @@ RSpec.describe 'GraphQL query: product(id: ID!)' do
           description: '新鮮なトマト',
           price: 150,
           imageUrl: %r{/tomato.jpg\z},
+          categories: [
+            { name: '野菜' },
+            { name: '実もの' },
+          ],
         },
       },
     )
