1108782587a7eb25bd2eed19d6d539aead3afc84
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-08-20 12:15:22 +0900

    Add gem 'factory_bot_rails'

--- minimart/spec/rails_helper.rb
+++ minimart/spec/rails_helper.rb
@@ -23,8 +23,10 @@ require 'rspec/rails'
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
