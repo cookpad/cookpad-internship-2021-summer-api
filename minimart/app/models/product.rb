48b2e2fd7559d1f1cd9692f86a7089cb07df711d
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2021-08-20 16:15:09 +0900

    Improve models

--- minimart/app/models/product.rb
+++ minimart/app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  has_many :order_items, dependent: :restrict_with_exception
 end
--- minimart/app/models/user.rb
+++ minimart/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   belongs_to :pickup_location, optional: true
+  has_many :orders, dependent: :restrict_with_exception
   has_many :paid_orders, -> { paid.order(:ordered_at) }, class_name: 'Order'
+
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
 end
